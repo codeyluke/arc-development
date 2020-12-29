003497ea8a2f9a0573a03d4a9ba355ef5a138f19
codeyluke <lukekennedy212@gmail.com>
2020-12-29 16:34:31 +0800

    adding the theme file to application

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from './ui/Theme';
 import Header from '../components/ui/Header';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       <h1>hello</h1>
-    </div>
+    </ThemeProvider>
   );
 }
 
