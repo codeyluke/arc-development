9473122981d7b79338ecf4f25d1db729bbea7ec8
codeyluke <lukekennedy212@gmail.com>
2021-01-01 02:22:11 +0800

    revolution block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,11 +7,14 @@ import Button from '@material-ui/core/Button';
 import ButtonArrow from '../components/ui/ButtonArrow';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import animationData from '../animations/landinganimation/data';
 import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
 import websitesIcon from '../assets/websiteIcon.svg';
+import revolutionBackground from '../assets/repeatingBackground.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -84,6 +87,28 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '12em',
     [theme.breakpoints.down('sm')]: {
       padding: 25,
+    },
+  },
+  revolutionBackground: {
+    backgroundImage: `url(${revolutionBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%',
+  },
+  revolutionCard: {
+    position: 'absolute',
+    boxShadow: theme.shadows[10],
+    borderRadius: 15,
+    padding: '10em',
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: '8em',
+      paddingBottom: '8em',
+      paddingRight: 0,
+      paddingLeft: 0,
+      borderRadius: 0,
+      width: '100%',
     },
   },
 }));
@@ -254,6 +279,46 @@ export default function LandingPage() {
               alt="website icon"
             />
           </Grid>
+        </Grid>
+      </Grid>
+      {/* ---------- Revolution Block ---------- */}
+      <Grid item>
+        <Grid
+          style={{ height: '100em', marginTop: '12em' }}
+          container
+          alignItems="center"
+          justify="center"
+        >
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                style={{ textAlign: 'center' }}
+              >
+                <Grid item>
+                  <Typography variant="h3" gutterBottom>
+                    The Revolution
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    Visionary insights coupled with cuttin-edge technology is a
+                    recipe for revolution.
+                  </Typography>
+                  <Button className={classes.learnButton} variant="outlined">
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={10}
+                      height={10}
+                      fill={theme.palette.common.blue}
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBackground} />
         </Grid>
       </Grid>
     </Grid>
