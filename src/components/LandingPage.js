56f08818fb4f9c7e8ac6f4fa8ed13162c37ff83b
codeyluke <lukekennedy212@gmail.com>
2021-01-01 01:55:13 +0800

    website development

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,6 +11,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import animationData from '../animations/landinganimation/data';
 import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
+import websitesIcon from '../assets/websiteIcon.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -178,7 +179,7 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       {/* ---------- iOS/Android Block ---------- */}
-      <Grid item>
+      <Grid item className={classes.serviceContainer}>
         <Grid
           container
           direction="row"
@@ -212,6 +213,45 @@ export default function LandingPage() {
               className={classes.icon}
               src={mobileAppsIcon}
               alt="custom software icon"
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* ---------- Website Block ---------- */}
+      <Grid item className={classes.serviceContainer}>
+        <Grid
+          container
+          direction="row"
+          justify={matchesSM ? 'center' : undefined}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant="h4">Website Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Reach More. Discover More. Sell More.
+            </Typography>
+            <Typography variant="subtitle1">
+              Optimized for Search Engines, built for speed.
+            </Typography>
+            <Button className={classes.learnButton} variant="outlined">
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              src={websitesIcon}
+              alt="website icon"
             />
           </Grid>
         </Grid>
