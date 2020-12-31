451e96c63d2e97dfff95c15e838a69ec7463021b
codeyluke <lukekennedy212@gmail.com>
2021-01-01 01:34:00 +0800

    custom software development

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,8 +6,10 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from '../components/ui/ButtonArrow';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import animationData from '../animations/landinganimation/data';
+import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -34,13 +36,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '1em',
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: 'none',
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
+    ...theme.typography.learnButton,
     fontSize: '0.9rem',
     height: 43,
     width: 145,
@@ -61,11 +57,40 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: 0,
     },
   },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange,
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    height: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
+  },
+  subtitle: {
+    marginBottom: '1em',
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25,
+    },
+  },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const defaultOptions = {
     loop: true,
@@ -78,6 +103,7 @@ export default function LandingPage() {
 
   return (
     <Grid container direction="column" className={classes.mainContainer}>
+      {/* ---------- Hero Block ---------- */}
       <Grid item>
         <Grid container justify="flex-end" alignItems="center" direction="row">
           <Grid sm item className={classes.heroTextContainer}>
@@ -108,6 +134,46 @@ export default function LandingPage() {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* ---------- Services Block ---------- */}
+      <Grid item className={classes.serviceContainer}>
+        <Grid
+          container
+          direction="row"
+          justify={matchesSM ? 'center' : undefined}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant="h4">Custom Software Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Save Energy. Save Time. Save Money.
+            </Typography>
+            <Typography variant="subtitle1">
+              Complete digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button className={classes.learnButton} variant="outlined">
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              src={customSoftwareIcon}
+              alt="custom software icon"
+            />
           </Grid>
         </Grid>
       </Grid>
